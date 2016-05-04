8a15daf1fa614c8d5b530f196afe27deaaa221cc
William Quan <William@Williams-MacBook-Pro.local>
2016-05-03 21:18:16 -0700

    finished midterm customization

---
+++ midterm-project-assignment-stndr1/ClearStyle/ToDoItem.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class ToDoItem: NSObject {
+    // A text description of this item.
+    var text: String
+    
+    // A Boolean value that determines the completed state of this item.
+    var completed: Bool
+    
+    // Returns a ToDoItem initialized with the given text and default completed value.
+    init(text: String) {
+        self.text = text
+        self.completed = false
+    }   
+}
